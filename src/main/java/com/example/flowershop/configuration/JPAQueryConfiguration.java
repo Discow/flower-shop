15d0875c70d1f2937558373760e9a4e5652af797
Discow <jsmail666666@gmail.com>
2024-05-30 20:13:43 +0800

    新增：引入QueryDSL依赖

---
+++ src/main/java/com/example/flowershop/configuration/JPAQueryConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.flowershop.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * 配置QueryDSL
+ */
+@Configuration
+public class JPAQueryConfiguration {
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    public JPAQueryConfiguration(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
