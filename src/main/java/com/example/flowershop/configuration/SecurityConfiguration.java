d149799672e42bf3d8732e7aa2b6d83a2acd3824
Discow <jsmail666666@gmail.com>
2024-06-05 20:02:32 +0800

    修改：优化管理员监控页面定时器的执行逻辑

--- src/main/java/com/example/flowershop/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/flowershop/configuration/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/login.html", "/register.html", "/forgot-password.html", "/api/auth/**").permitAll() //放行登录验证相关的页面和api
                 .antMatchers("/api/customer/mainPage/**", "/index.html").permitAll() //放行首页
-                .antMatchers("/res/**", "/favicon.ico").permitAll() //放行静态资源
+                .antMatchers("/res/**", "/subpage/**", "/favicon.ico").permitAll() //放行静态资源
                 .regexMatchers("/admin[\\w\\-.]*").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
