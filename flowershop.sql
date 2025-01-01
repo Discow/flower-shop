6bca1daeac9e92aecfae8aad703c7449dca88674
Discow <73215520+Discow@users.noreply.github.com>
2025-01-01 13:39:50 +0800

    Add flowershop.sql

---
+++ flowershop.sql
@@ -0,0 +1,210 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 8.4.1 : Database - flower_shop_web
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`flower_shop_web` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `flower_shop_web`;
+
+/*Table structure for table `comment` */
+
+DROP TABLE IF EXISTS `comment`;
+
+CREATE TABLE `comment` (
+  `content` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci,
+  `rating` int NOT NULL,
+  `time` datetime NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
+  `flower_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`flower_id`),
+  KEY `comment_flower_null_fk` (`flower_id`),
+  CONSTRAINT `comment_flower_null_fk` FOREIGN KEY (`flower_id`) REFERENCES `flower` (`id`),
+  CONSTRAINT `comment_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `comment` */
+
+/*Table structure for table `favorite` */
+
+DROP TABLE IF EXISTS `favorite`;
+
+CREATE TABLE `favorite` (
+  `user_id` int NOT NULL DEFAULT '0',
+  `flower_id` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`,`flower_id`),
+  KEY `favorite_flower_null_fk` (`flower_id`),
+  CONSTRAINT `favorite_flower_null_fk` FOREIGN KEY (`flower_id`) REFERENCES `flower` (`id`),
+  CONSTRAINT `favorite_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `favorite` */
+
+/*Table structure for table `flower` */
+
+DROP TABLE IF EXISTS `flower`;
+
+CREATE TABLE `flower` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `price` decimal(5,2) NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `picture` longblob,
+  `inventory` int NOT NULL DEFAULT '0',
+  `status` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `add_time` datetime DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `flower_flower_category_null_fk` (`category_id`),
+  CONSTRAINT `flower_flower_category_null_fk` FOREIGN KEY (`category_id`) REFERENCES `flower_category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `flower` */
+
+/*Table structure for table `flower_category` */
+
+DROP TABLE IF EXISTS `flower_category`;
+
+CREATE TABLE `flower_category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `flower_category` */
+
+/*Table structure for table `login_record` */
+
+DROP TABLE IF EXISTS `login_record`;
+
+CREATE TABLE `login_record` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `time` datetime DEFAULT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `login_record_user_null_fk` (`user_id`),
+  CONSTRAINT `login_record_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `login_record` */
+
+/*Table structure for table `logistics` */
+
+DROP TABLE IF EXISTS `logistics`;
+
+CREATE TABLE `logistics` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `company` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `consignor` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `receiver` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `estimated_time` datetime DEFAULT NULL,
+  `order_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `logistics_order_null_fk` (`order_id`),
+  CONSTRAINT `logistics_order_null_fk` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `logistics` */
+
+/*Table structure for table `logistics_timeline` */
+
+DROP TABLE IF EXISTS `logistics_timeline`;
+
+CREATE TABLE `logistics_timeline` (
+  `id` int NOT NULL DEFAULT '0',
+  `status` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `update_time` datetime NOT NULL,
+  `logistics_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `logistics_timeline_logistics_null_fk` (`logistics_id`),
+  CONSTRAINT `logistics_timeline_logistics_null_fk` FOREIGN KEY (`logistics_id`) REFERENCES `logistics` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `logistics_timeline` */
+
+/*Table structure for table `order` */
+
+DROP TABLE IF EXISTS `order`;
+
+CREATE TABLE `order` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `total_amount` decimal(20,2) NOT NULL,
+  `payment_type` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `receive_type` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `note` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `time` datetime NOT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_user_null_fk` (`user_id`),
+  CONSTRAINT `order_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `order` */
+
+/*Table structure for table `order_detail` */
+
+DROP TABLE IF EXISTS `order_detail`;
+
+CREATE TABLE `order_detail` (
+  `order_id` int NOT NULL DEFAULT '0',
+  `flower_id` int NOT NULL DEFAULT '0',
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`flower_id`),
+  KEY `order_detail_flower_null_fk` (`flower_id`),
+  CONSTRAINT `order_detail_flower_null_fk` FOREIGN KEY (`flower_id`) REFERENCES `flower` (`id`),
+  CONSTRAINT `order_detail_order_null_fk` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `order_detail` */
+
+/*Table structure for table `persistent_logins` */
+
+DROP TABLE IF EXISTS `persistent_logins`;
+
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `series` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `token` varchar(64) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `persistent_logins` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `role` enum('USER','ADMIN') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_pk` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`email`,`username`,`password`,`phone`,`role`) values (-1,'deleted','已删除的用户','0','0','USER'),(1,'admin@admin.com','测试管理员','$2a$10$KKSQDnGgMB.gpG/dyFVjUuj0g/M5qFYvXKz1xTfBlywgwfs8ID6V2','13733332222','ADMIN'),(2,'test@user.com','测试用户','$2a$10$9GMx.8oVssUefked/.nQPOv3H65MCVHlS8WnAFolvyELY38Rh2dfO','18038483827','USER');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
